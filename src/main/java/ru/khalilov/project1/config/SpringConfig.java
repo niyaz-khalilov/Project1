1398b5d7b179e34040c0a66b2fac334a90130a61
niyaz-khalilov <nkhalilovv@gmail.com>
2022-09-13 00:04:26 +0300

    SpringConfig was updated

--- src/main/java/ru/khalilov/project1/config/SpringConfig.java
+++ src/main/java/ru/khalilov/project1/config/SpringConfig.java
@@ -64,7 +64,7 @@ public class SpringConfig implements WebMvcConfigurer {
         dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
         dataSource.setUrl(environment.getProperty("url"));
         dataSource.setUsername(environment.getProperty("user"));
-        dataSource.setPassword("password");
+        dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
     }
 
